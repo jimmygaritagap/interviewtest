a586d2e981b9ae2efd1b6a389b657dfa0c52b4c6
Jimmy Garita <jgarita@saatvamattress.com>
2021-06-23 14:17:26 -0600

    EX-01 Adding php exercises

---
+++ practice/php/interview_a.php
@@ -0,0 +1,23 @@
+<?php
+/*
+	Create a function called generateArray() that
+	generates an array of random integers, where the
+	length of the array and the maximum random value are
+	defined as parameters. For example, generateArray(5, 3)
+	may generate the array [3, 2, 0, 1, 1]. Random values
+	should be between 0 and the maximum random value inclusive.
+*/
+class InterviewA
+{
+
+    function generate_array()
+    {
+        return 'hello';
+    }
+
+}
+
+$interview = new InterviewA();
+$result = $interview->generate_array(5,3);
+
+// example: output: [3, 2, 0, 1, 1]
